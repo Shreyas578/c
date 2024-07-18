df0f01597633154ca3f64d65bc1ec82e477e718d
Shreyas578 <nikamsarita21@gmail.com>
2024-07-18 05:26:41 +0000

    done

---
+++ basic.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("Enter a number\n");
+    scanf("%d",&a);
+    printf("Enter the number\n");
+    scanf("%d",&b);
+    int sum=a+b;
+    printf("Sum=%d",sum);
+}
